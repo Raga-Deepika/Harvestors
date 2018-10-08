bdbfc304062135cf908c407c0d971ad0154382b0
Raga-Deepika <raga@zinnov.com>
2018-10-08 16:35:13 +0530

    Changes added to the file

--- entrepreneur/base.py
+++ entrepreneur/base.py
@@ -6,15 +6,6 @@ import requests
 import random
 import logging
 
-#
-# def PrintException():
-#     exc_type, exc_obj, tb = sys.exc_info()
-#     f = tb.tb_frame
-#     lineno = tb.tb_lineno
-#     filename = f.f_code.co_filename
-#     linecache.checkcache(filename)
-#     line = linecache.getline(filename, lineno, f.f_globals)
-#     print('EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj))
 
 
 logger = logging.getLogger(__name__)
@@ -153,4 +144,3 @@ def entre_base(category, page):
         return None
 
 
-# print(entre_base(category='patents',page='1'))
